59569c4790ca2adcb56d5d58ac7b5f1081fbf12c
walterwhiteop77 <walterwhite2k27@gmail.com>
2025-09-22 20:49:26 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# config/__init__.py
+"""Configuration package for the Telegram bot."""
+
+# handlers/__init__.py
+"""Handler package for bot commands and messages."""
+
+# services/__init__.py
+"""Service package for external API interactions."""
+
+# models/__init__.py
+"""Data models package."""
+
+# utils/__init__.py
+"""Utility functions and decorators."""
